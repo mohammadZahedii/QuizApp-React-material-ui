c84208cb6a5aa95c5b4e3b466138ed3ad3e04683
mohammadZahedii <mohammadzahedi_1995@yahoo.com>
2021-10-13 22:02:40 +0330

    customize Home.js component

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,11 +1,33 @@
 import React from 'react'
-
-
+import Box from '@mui/material/Box'
+import Image from './quiz.png'
+import Typography from '@mui/material/Typography'
+import {TextField} from '@mui/material'
+import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
 
 const Home =()=>{
 
     return(
-        <di>Home Page</di>
+        <Grid container spacing={1} sx={{flexDirection:['column','column','row-reverse']}}>
+            <Grid item xs={12} md={6}>
+                <img src={Image} style={{width:'100%'}} />
+            </Grid>
+            <Grid item xs={12} md={6}>
+                <Box sx={{p:t=>t.spacing(1),my:t=>t.spacing(1),mr:t=>t.spacing(1),display:'flex',flexDirection:'column',alignItems:'center',width:'100%'}}>
+                    <Typography gutterBottom component="span" variant="h4" sx={{display:'flex',justifyContent:'center'}}>Quiz Settings</Typography>
+                    <Box sx={{width:'100%',m:t=>t.spacing(1),px:t=>t.spacing(2)}}>
+                        <TextField fullWidth label="Enter your Name" variant="outlined" sx={{mb:t=>t.spacing(2)}}/>
+                        <TextField fullWidth select label="Select Category" variant="outlined" sx={{mb:t=>t.spacing(2)}}/>
+                        <TextField fullWidth select label="Select Difficulty" variant="outlined" sx={{mb:t=>t.spacing(2)}}/>
+                        <Button variant="contained" color="primary" fullWidth size="large">
+                            Start Quiz
+                        </Button>
+                    </Box>
+                </Box>
+            </Grid>
+     
+        </Grid>
     )
 }
 
