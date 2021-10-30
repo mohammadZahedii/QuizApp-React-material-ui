6610ffbe49d8612ef4532a9cdde2c4e65854a0f2
mohammadZahedii <mohammadzahedi_1995@yahoo.com>
2021-10-30 09:38:37 +0330

    add provider,store & reducer in index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {provider} from 'react-redux'
+import {createStore} from 'redux'
+import reducer from './reducer'
+
+const store =createStore(reducer)
 
 ReactDOM.render(
-  
+  <provider store={store}>
     <App />
+  </provider>
   ,
   document.getElementById('root')
 );
