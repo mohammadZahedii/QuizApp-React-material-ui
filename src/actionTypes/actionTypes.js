f33187ef5060a964d08142a1a42a008cfbf1a36d
mohammadZahedii <mohammadzahedi_1995@yahoo.com>
2021-10-30 09:40:27 +0330

    Add action creator for name

---
+++ src/actionTypes/actionTypes.js
@@ -0,0 +1,1 @@
+export const CHANGE_NAME_VALUE='CHANGE_NAME_VALUE'
---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+import * as types from './../actionTypes/actionTypes'
+
+
+export const changeValue=(value)=>({
+    type:types.CHANGE_NAME_VALUE,
+    value
+})
